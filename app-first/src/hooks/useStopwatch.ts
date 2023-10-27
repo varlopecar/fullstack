468a740f206d1665a1ece83a3509c471456106e4
Carlos Vargas <varlopecar@gmail.com>
2023-10-27 09:15:11 +0200

    chore: finish stopwatch hook

---
+++ app-first/src/hooks/useStopwatch.ts
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+
+const useStopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const interval = useRef<ReturnType<typeof setInterval>>();
+
+  useEffect(() => {
+    if (isRunning) {
+      interval.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval.current);
+    }
+    return () => clearInterval(interval.current);
+  }), [isRunning];
+
+  const start = () => {
+    setIsRunning(true);
+  };
+
+  const stop = () => {
+    setIsRunning(false);
+  };
+
+  const reset = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  const minutes = Math.floor(time / 1000 / 60);
+  const seconds = Math.floor(time / 1000 - minutes * 60);
+  const miliseconds = time - minutes * 60 * 1000 - seconds * 1000;
+
+  return { start, stop, reset, minutes, seconds, miliseconds };
+};
+
+export default useStopwatch;
